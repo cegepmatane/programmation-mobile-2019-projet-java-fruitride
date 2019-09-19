fb4303d71d149e8968f0c68f74db798612278a5a
BigLazyBrother <tcousson@wanadoo.fr>
2019-09-19 17:48:51 -0400

    Ajout de l'activité de conficguration de l'appli.

--- app/src/main/java/ca/qc/cgmatane/fruitride/vue/VueCarte.java
+++ app/src/main/java/ca/qc/cgmatane/fruitride/vue/VueCarte.java
@@ -2,7 +2,10 @@ package ca.qc.cgmatane.fruitride.vue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,6 +35,16 @@ public class VueCarte extends AppCompatActivity implements OnMapReadyCallback {
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(mapViewBundle);
         mapView.getMapAsync(this);
+
+        Button bouton = findViewById(R.id.button2);
+
+        final Intent testConfiguration = new Intent(this, VueConfiguration.class);
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(testConfiguration);
+            }
+        });
     }
     @Override
     public void onSaveInstanceState(Bundle outState) {
---
+++ app/src/main/java/ca/qc/cgmatane/fruitride/vue/VueConfiguration.java
@@ -0,0 +1,16 @@
+package ca.qc.cgmatane.fruitride.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.qc.cgmatane.fruitride.R;
+
+public class VueConfiguration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_configuration);
+    }
+}
