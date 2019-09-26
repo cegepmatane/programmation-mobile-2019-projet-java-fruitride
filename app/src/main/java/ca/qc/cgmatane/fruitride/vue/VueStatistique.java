6518f3b5161cfb53ace7fc30cd380aa67dec55da
Loic Hug <h-loic@orange.fr>
2019-09-26 17:44:55 -0400

    modification de l'histogramme, utilisation de anyChart

--- app/src/main/java/ca/qc/cgmatane/fruitride/vue/VueStatistique.java
+++ app/src/main/java/ca/qc/cgmatane/fruitride/vue/VueStatistique.java
@@ -1,57 +1,56 @@
 package ca.qc.cgmatane.fruitride.vue;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
 
 import java.util.ArrayList;
 
 import ca.qc.cgmatane.fruitride.R;
 
 public class VueStatistique extends AppCompatActivity {
-    BarChart barChart;
-    BarData barData;
-    BarDataSet barDataSet;
-    ArrayList barEntries;
+    private AnyChartView columnChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_statistique);
-        barChart = findViewById(R.id.BarChart);
-        getEntries();
-        barDataSet = new BarDataSet(barEntries, "");
-        barData = new BarData(barDataSet);
-        barChart.setData(barData);
-        //barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-        barDataSet.setValueTextColor(Color.BLACK);
-        barDataSet.setValueTextSize(18f);
-
-        Button boutonRetour = findViewById(R.id.boutonRetour);
-        final Intent intentRetour = new Intent(this, Accueil.class);
-        boutonRetour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intentRetour);
-            }
-        });
+        columnChart = (AnyChartView)findViewById(R.id.columnChart);
+        columnChart.setProgressBar(findViewById(R.id.progress_bar));
+        Cartesian cartesian = AnyChart.column();
+        Column column = cartesian.column(getData());
+        column.tooltip()
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d);
+        cartesian.animation(true);
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+        cartesian.yAxis(0).title("nombre de pas");
+        columnChart.setChart(cartesian);
     }
-    private void getEntries() {
-        barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1, 3000));
-        barEntries.add(new BarEntry(2, 5500));
-        barEntries.add(new BarEntry(3, 2000));
-        barEntries.add(new BarEntry(4, 10523));
-        barEntries.add(new BarEntry(5, 5124));
-        barEntries.add(new BarEntry(6, 523));
+
+    private ArrayList getData(){
+        ArrayList<DataEntry> entries = new ArrayList<>();
+        entries.add(new ValueDataEntry("lundi", 85671));
+        entries.add(new ValueDataEntry("mardi", 93140));
+        entries.add(new ValueDataEntry("mercredi", 124630));
+        entries.add(new ValueDataEntry("jeudi", 110430));
+        entries.add(new ValueDataEntry("vendredi", 128000));
+        entries.add(new ValueDataEntry("samedi", 143550));
+        entries.add(new ValueDataEntry("dimanche", 120660));
+        return entries;
     }
 }
