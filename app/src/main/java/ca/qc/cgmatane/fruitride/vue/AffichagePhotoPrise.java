8ce5714b6785199539dd5fed0fdd20c9d94ca485
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-10-12 22:39:05 -0400

    Enregistrement photos sur un serveur si l'utilisateur le veut

--- app/src/main/java/ca/qc/cgmatane/fruitride/vue/AffichagePhotoPrise.java
+++ app/src/main/java/ca/qc/cgmatane/fruitride/vue/AffichagePhotoPrise.java
@@ -2,28 +2,39 @@ package ca.qc.cgmatane.fruitride.vue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.HashMap;
 
 import ca.qc.cgmatane.fruitride.R;
 import ca.qc.cgmatane.fruitride.controleur.ControleurAffichagePhotoPrise;
 import ca.qc.cgmatane.fruitride.controleur.ControleurCarte;
+import ca.qc.cgmatane.fruitride.reseau.RequestHandler;
 
 public class AffichagePhotoPrise extends AppCompatActivity implements VueAffichagePhotoPrise{
 
     protected ControleurAffichagePhotoPrise controleurAffichagePhotoPrise =
             new ControleurAffichagePhotoPrise(this);
     protected ImageView imageView;
+
+    public static final String UPLOAD_URL = "https://test-qr-response.real-it.duckdns.org/upload_image.php";
+    public static final String UPLOAD_KEY = "image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,14 @@ public class AffichagePhotoPrise extends AppCompatActivity implements VueAfficha
                 controleurAffichagePhotoPrise.ajouterImageALaGallerie();
             }
         });
+
+        Button affichagePhotoPriseBoutonPartager = findViewById(R.id.affichage_photo_prise_bouton_partager);
+        affichagePhotoPriseBoutonPartager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                partagerImage();
+            }
+        });
     }
 
     @Override
@@ -56,7 +75,75 @@ public class AffichagePhotoPrise extends AppCompatActivity implements VueAfficha
 
     @Override
     public void partagerImage() {
+        //TODO : nettoyer le code
+        class UploadImage extends AsyncTask<Bitmap,Void,String> {
+            ProgressDialog loading;
+            RequestHandler rh = new RequestHandler();
 
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(AffichagePhotoPrise.this, "Partage de l'image", "Veuillez attendre ...",true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Bitmap... params) {
+                Bitmap bitmap = params[0];
+                String uploadImage = getStringImage(bitmap);
+
+                HashMap<String,String> data = new HashMap<>();
+                data.put(UPLOAD_KEY, uploadImage);
+                data.put("name",getFileName(controleurAffichagePhotoPrise.getCheminImage()));
+
+                String result = rh.postRequest(UPLOAD_URL,data);
+                return result;
+            }
+        }
+
+        UploadImage ui = new UploadImage();
+        ui.execute(BitmapFactory.decodeFile(controleurAffichagePhotoPrise.getCheminImage()));
+    }
+
+    String getFileName(String chemin) {
+        String[] tab = chemin.split("/");
+        return tab[tab.length-1];
+    }
+
+    String getFileName(Uri uri){
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
     }
 
     @Override
